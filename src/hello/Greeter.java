41c0439eaa6caf0c2af55b3d295c82dd7c22dd07
wangjinyu <wangjy0@163.com>
2017-05-23 19:06:25 +0800

    init

---
+++ src/hello/Greeter.java
@@ -0,0 +1,9 @@
+package hello;
+
+public class Greeter {
+	
+	public void greet(String name){
+		System.out.println("Hello, " + name);
+	}
+
+}
---
+++ src/hello/Main.java
@@ -0,0 +1,11 @@
+package hello;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Greeter g = new Greeter();
+		g.greet(args[0]);
+	}
+
+}
