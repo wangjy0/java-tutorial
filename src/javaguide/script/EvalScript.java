7eacd3e4e83833679cb429db6b28254c007e63f2
wangjinyu <jinyu5@jiaju.com>
2017-07-10 17:44:05 +0800

    7.10

--- src/javaguide/script/EvalScript.java
+++ src/javaguide/script/EvalScript.java
@@ -1,20 +1,36 @@
 package javaguide.script;
 
+import java.util.List;
+
 import javax.script.ScriptEngine;
+import javax.script.ScriptEngineFactory;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
 public class EvalScript {
 	public static void main(String[] args) throws Exception {
+		ScriptEngineManager manager = new ScriptEngineManager();  
+		List<ScriptEngineFactory> factories = manager.getEngineFactories();  
+		for (ScriptEngineFactory f : factories) {  
+		    System.out.println("egine name:" + f.getEngineName() + "\n"  
+		            + "engine version:" + f.getEngineVersion() + "\n"  
+		            + "language name:" + f.getLanguageName());  
+		}  
+		
+		
 		// create a script engine manager
 		ScriptEngineManager sem = new ScriptEngineManager();
 		// create a Nashorn script engine
-		ScriptEngine se = sem.getEngineByExtension("js");//getEngineByName("nashorn");
+		ScriptEngine se = sem.getEngineByName("javascript");
 		// evaluate JavaScript statement
 		try {
-			se.eval("print('Hello, World!');");
+			se.put("a", 4);  
+			se.put("b", 6);  
+			Object val = se.eval("a+b");
+			System.out.println(val);
 		} catch (final ScriptException e) {
 			e.printStackTrace();
 		}
+		
 	}
 }
